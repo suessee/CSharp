69b044e57be43165d21d26c724b45a2ebe76083b
suessee <63084918+suessee@users.noreply.github.com>
2022-06-29 10:18:51 +0900

    [Bronze III] Title: A+B - 7, Time: 68 ms, Memory: 5996 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder answer = new StringBuilder();
+
+            int c = int.Parse(Console.ReadLine());
+
+            int[] a = new int[c];
+            int[] b = new int[c];
+
+            for (int i = 0; i < c; i++)
+            {
+                String[] aAndB = Console.ReadLine().Split();
+                a[i] = int.Parse(aAndB[0]);
+                b[i] = int.Parse(aAndB[1]);
+            }
+            for (int i = 0; i < c; i++)
+            {
+                answer.Append("Case #" + (i+1) + ": ").Append(a[i] + b[i]).AppendLine("");
+            }
+
+            Console.WriteLine(answer.ToString());
+        }
+    }
+}
