5523edf749b16479c10562af18591006ec240ddf
suessee <63084918+suessee@users.noreply.github.com>
2022-06-29 10:01:30 +0900

    [Bronze V] Title: ??!, Time: 72 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String aa = Console.ReadLine();
+            
+            Console.WriteLine(aa + "??!");
+        }
+    }
+}
