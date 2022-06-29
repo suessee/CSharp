d912e277ac931f77e887c15d8d73ec375fe38254
suessee <63084918+suessee@users.noreply.github.com>
2022-06-29 09:53:39 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
