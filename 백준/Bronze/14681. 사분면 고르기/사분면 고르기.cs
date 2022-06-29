d6d3b65020a53d7718c02d2284620d66c98ca543
suessee <63084918+suessee@users.noreply.github.com>
2022-06-29 10:05:34 +0900

    [Bronze V] Title: 사분면 고르기, Time: 60 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (a < 0 && b > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else if (a < 0 && b < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else if (a > 0 && b < 0)
+            {
+                Console.WriteLine("4");
+            }
+
+        }
+    }
+}
