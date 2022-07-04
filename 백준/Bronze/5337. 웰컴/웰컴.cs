36f42c0558e87eeaab6d2e3f384bed4582608a80
suessee <63084918+suessee@users.noreply.github.com>
2022-07-04 16:36:13 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(".  .   .");
+            WriteLine("|  | _ | _. _ ._ _  _");
+            WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+
+        }
+    }
+}
