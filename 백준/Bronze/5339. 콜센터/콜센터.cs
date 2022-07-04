197a4592ce84ccb2e592e8c259ad41c9f5b65a11
suessee <63084918+suessee@users.noreply.github.com>
2022-07-04 16:44:10 +0900

    [Bronze V] Title: 콜센터, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,23 @@
+using System;
+using static System.Console;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("     /~\\");
+            WriteLine("    ( oo|");
+            WriteLine("    _\\=/_");
+            WriteLine("   /  _  \\");
+            WriteLine("  //|/.\\|\\\\");
+            WriteLine(" ||  \\ /  ||");
+            WriteLine("============");
+            WriteLine("|          |");
+            WriteLine("|          |");
+            WriteLine("|          |");
+        }
+    }
+}
