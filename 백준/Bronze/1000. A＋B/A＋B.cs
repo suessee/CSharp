8b6601588a3b6ef93e3846e111de7e3cd0c85c2f
suessee <63084918+suessee@users.noreply.github.com>
2022-07-04 09:39:18 +0900

    [Bronze V] Title: A+B, Time: 60 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String aa = Console.ReadLine();
+            String[] bb = aa.Split();
+            int a = int.Parse(bb[0]), b = int.Parse(bb[1]); 
+            Console.WriteLine(a+b);
+        }
+    }
+}
