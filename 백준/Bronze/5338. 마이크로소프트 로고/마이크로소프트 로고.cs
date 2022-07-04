979b5c26b652076d38953570f654c4e6e8360d11
suessee <63084918+suessee@users.noreply.github.com>
2022-07-04 16:38:22 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 56 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("       _.-;;-._");
+            WriteLine("'-..-'|   ||   |");
+            WriteLine("'-..-'|_.-;;-._|");
+            WriteLine("'-..-'|   ||   |");
+            WriteLine("'-..-'|_.-''-._|");
+        }
+    }
+}
