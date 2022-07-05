97cbfff52bcad81b7236e04c3ecb74ac64d2dca4
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 15:06:30 +0900

    [Bronze V] Title: 새로운 시작, Time: 56 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("파이팅!!");
+        }
+    }
+}
