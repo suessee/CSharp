15f310ffecc56bf7a610320d2041c430d81765eb
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 11:24:26 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 60 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("고려대학교");
+        }
+    }
+}
