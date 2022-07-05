f4b2a9f1bb49744fa6f373494c147fa3770f99c5
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 16:37:42 +0900

    [Bronze V] Title: The World Responds, Time: 56 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            WriteLine("The world says hello!");
+        }
+    }
+}
