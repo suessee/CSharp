e67913b8c93c8ecf3a61156810003e8d26214e03
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 15:39:44 +0900

    [Bronze V] Title: Арифметическая магия, Time: 80 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(ReadLine());
+            WriteLine(1);
+        }
+    }
+}
