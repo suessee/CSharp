3fd2e4274f761cbbf7d8714b6e5ce7b46960262c
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 10:37:29 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 56 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cs
@@ -0,0 +1,23 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("    8888888888  888    88888");
+            WriteLine("   88     88   88 88   88  88");
+            WriteLine("    8888  88  88   88  88888 ");
+            WriteLine("       88 88 888888888 88   88");
+            WriteLine("88888888  88 88     88 88    888888");
+            WriteLine();
+            WriteLine("88  88  88   888    88888    888888");
+            WriteLine("88  88  88  88 88   88  88  88");
+            WriteLine("88 8888 88 88   88  88888    8888");
+            WriteLine(" 888  888 888888888 88  88      88");
+            WriteLine("  88  88  88     88 88   88888888");
+        }
+    }
+}
