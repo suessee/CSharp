69e1955e9f0ebde7cd4bd2584479cc05d76590ea
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 10:42:27 +0900

    [Bronze V] Title: NFC West vs North, Time: 56 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cs
@@ -0,0 +1,25 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("NFC West       W   L  T");
+            WriteLine("-----------------------");
+            WriteLine("Seattle        13  3  0");
+            WriteLine("San Francisco  12  4  0");
+            WriteLine("Arizona        10  6  0");
+            WriteLine("St. Louis      7   9  0");
+            WriteLine();
+            WriteLine("NFC North      W   L  T");
+            WriteLine("-----------------------");
+            WriteLine("Green Bay      8   7  1");
+            WriteLine("Chicago        8   8  0");
+            WriteLine("Detroit        7   9  0");
+            WriteLine("Minnesota      5  10  1");
+        }
+    }
+}
