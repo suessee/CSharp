598260a08eec92045b6ed26790034d6ba62ff591
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 10:39:53 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 52 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+            WriteLine("N2 Bomber      Heavy Fighter  Limited    21        ");
+            WriteLine("J-Type 327     Light Combat   Unlimited  1         ");
+            WriteLine("NX Cruiser     Medium Fighter Limited    18        ");
+            WriteLine("N1 Starfighter Medium Fighter Unlimited  25        ");
+            WriteLine("Royal Cruiser  Light Combat   Limited    4         ");          
+        }
+    }
+}
