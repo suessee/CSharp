de958efd456832ddac14eda96a0abfbf90a761c0
suessee <63084918+suessee@users.noreply.github.com>
2022-07-05 15:09:37 +0900

    [Bronze V] Title: 이상한 기호, Time: 68 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+using System.Numerics;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = ReadLine().Split();
+            BigInteger A = int.Parse(num[0]), B = BigInteger.Parse(num[1]);
+            WriteLine((A + B) * (A - B));
+        }
+    }
+}
