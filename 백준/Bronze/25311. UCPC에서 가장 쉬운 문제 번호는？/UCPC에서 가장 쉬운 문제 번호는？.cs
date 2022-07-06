853e366470d33c998f9ae1bee232bdb54c046a80
suessee <63084918+suessee@users.noreply.github.com>
2022-07-06 10:57:21 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.Parse(ReadLine());
+
+            WriteLine("A");
+        }
+    }
+}
