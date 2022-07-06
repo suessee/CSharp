2383a21366bdbf1410383003c65dfd55dfcf284c
suessee <63084918+suessee@users.noreply.github.com>
2022-07-06 10:13:20 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int n = int.Parse(ReadLine());
+
+            WriteLine(1 + "\n" + 0);
+        }
+    }
+}
