7e2f28a5f2a8dd3aff64a09617b7272c3d2cd557
suessee <63084918+suessee@users.noreply.github.com>
2022-07-08 14:40:39 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+
+namespace FirstProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = ReadLine();
+            WriteLine(":fan::fan::fan:");
+            WriteLine(":fan::"+ input +"::fan:");
+            WriteLine(":fan::fan::fan:");
+        }
+    }
+}
