8e9a2475853dc2f01186cce1ec2394e930326535
suessee <63084918+suessee@users.noreply.github.com>
2022-07-08 15:46:05 +0900

    [Bronze IV] Title: FA, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+
+
+namespace FirstProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string x = ReadLine();
+            WriteLine("FA");
+
+        }
+    }
+}
