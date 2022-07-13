2111c52651b7adc0e26045edd86d6b3ccb2e6c6c
suessee <63084918+suessee@users.noreply.github.com>
2022-07-13 14:51:34 +0900

    [Bronze IV] Title: Archivist, Time: 64 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cs
@@ -0,0 +1,121 @@
+using System;
+using static System.Console;
+
+namespace BaekJoon
+
+{
+
+    public static class Program
+
+    {
+
+        public static void Main()
+
+        {
+            int y = int.Parse(ReadLine());
+            switch (y)
+            {
+                case 1995:
+                    WriteLine("ITMO");
+                    break;
+
+                case 1996:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 1997:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 1998:
+                    WriteLine("ITMO");
+                    break;
+
+                case 1999:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2000:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 2001:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2002:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2003:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2004:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2005:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2006:
+                    WriteLine("PetrSU, ITMO");
+                    break;
+
+                case 2007:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 2008:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 2009:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2010:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2011:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2012:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2013:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 2014:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2015:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2016:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2017:
+                    WriteLine("ITMO");
+                    break;
+
+                case 2018:
+                    WriteLine("SPbSU");
+                    break;
+
+                case 2019:
+                    WriteLine("ITMO");
+                    break;
+
+            }
+        }
+    }
+}
