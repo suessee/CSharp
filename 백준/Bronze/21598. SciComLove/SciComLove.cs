76d7c5e3902158cfc1cfb831517b3d3166889580
suessee <63084918+suessee@users.noreply.github.com>
2022-07-15 10:01:13 +0900

    [Bronze IV] Title: SciComLove, Time: 64 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,23 @@
+using System;
+using static System.Console;
+
+
+namespace BaekJoon
+
+{
+
+    public static class Program
+
+    {
+
+        public static void Main()
+
+        {
+            int N = int.Parse(ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                WriteLine("SciComLove");
+            }
+        }
+    }
+}
