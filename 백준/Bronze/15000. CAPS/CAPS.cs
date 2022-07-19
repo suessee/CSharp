742dbcf6a3e42a097a3a7da26bf1a20aa3632e2a
suessee <63084918+suessee@users.noreply.github.com>
2022-07-19 16:24:44 +0900

    [Bronze IV] Title: CAPS, Time: 244 ms, Memory: 12440 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cs
@@ -0,0 +1,19 @@
+using System;
+using static System.Console;
+
+namespace BaekJoon
+
+{
+
+    public static class Program
+
+    {
+
+        public static void Main()
+
+        {
+            string n = ReadLine();
+            WriteLine(n.ToUpper());
+        }
+    }
+}
